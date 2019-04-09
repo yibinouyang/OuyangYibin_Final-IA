b8c496f7f728e8777b0a481a8b477b2dd9bbc2cc
yibinouyang <cyrene2717@outlook.com>
2019-04-08 22:40:19 -0400

    add js function

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var i =1;
+function nextImg(){
+    i++;
+    var next = document.getElementById("nImg");
+    next.src = "images/" +i+".jpg";
+    if(i==4){
+        i=0 ;
+    }
+}
+
+var Arraycolor = new Array("#00FF66", "#FFFF99", "#99CCFF", "#FFCCFF", "#FFCC99", "#00FFFF", "#FFFF00", "#FFCC00", "#FF00FF");
+        var n = 0;
+        function turncolors()
+        {
+            n++;
+            if (n == (Arraycolor.length - 1)) n = 0;
+            document.bgColor = Arraycolor[n];
+            setTimeout("turncolors()", 1000);
+        }
+        turncolors();

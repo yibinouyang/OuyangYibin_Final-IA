4e8d682a7486e0026444d6ee0099c0d26f40bdc0
yibinouyang <cyrene2717@outlook.com>
2019-04-12 21:01:12 -0400

    change two pictures
    changed two pictures and put Newsletters and Links together

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-
+let nextbtn = document.getElementById("NextImg");
+	nextbtn.addEventListener("click",nextImg);
 var i = 1;
 
 function nextImg() {
@@ -10,42 +11,4 @@ function nextImg() {
 	}
 }
 
-var Arraycolor = new Array("#00FF66", "#FFFF99", "#99CCFF", "#FFCCFF", "#FFCC99", "#00FFFF", "#FFFF00", "#FFCC00", "#FF00FF");
-var n = 0;
-
-function turncolors() {
-	n++;
-	if(n == (Arraycolor.length - 1)) n = 0;
-	document.bgColor = Arraycolor[n];
-	setTimeout("turncolors()", 1000);
-}
-turncolors();
-window.onload = function() {
-	var popupImg = document.getElementById("popupImg");
-	//popupImg.addEventListener('click', showMsg, false);
-	popupImg.onclick=function() {
-		var popup = document.getElementById("popup");
-		 popup.style.display = "block";
-	}
-	
-	var xdiv = document.getElementById("xdiv");
-	//popupImg.addEventListener('click', showMsg, false);
-	xdiv.onclick=function() {
-		var popup = document.getElementById("popup");
-		 popup.style.display = "none";
-	}
-	
-}
-
-function showMsg() {
-
-}
-
-function classToggle() {
-	const navs = document.querySelectorAll('.Navbar__Items')
-	
-	navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-  }
-  document.querySelector('.Navbar__Link-toggle')
-	.addEventListener('click', classToggle);
   

0fde4e805e49b6f8f84c7cbe75425180d5e12dd5
yibinouyang <cyrene2717@outlook.com>
2019-04-14 18:04:04 -0400

    Beautify the web
    Beautify the web

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
-        function slide_nav(){
-            var display = document.getElementById("mainNav").style.display;
-            if (display == 'none') {
-                document.getElementById("mainNav").style.display = 'block';
-            } else {
-                document.getElementById("mainNav").style.display = 'none';
-            }
-            
-        }
+
 
         let nextbtn = document.getElementById("NextImg");
 	nextbtn.addEventListener("click",nextImg);
